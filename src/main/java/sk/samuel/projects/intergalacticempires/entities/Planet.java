f797e201db0d4e178ee5e010ab1747c4157345f1
samuel <samuel14sromovsky@gmail.com>
2022-06-29 23:17:04 +0200

    created packages and 3 entities.

---
+++ src/main/java/sk/samuel/projects/intergalacticempires/entities/Planet.java
@@ -0,0 +1,20 @@
+package sk.samuel.projects.intergalacticempires.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Planet {
+    private Long id;
+    private String name;
+    private int populationOfPlanet;
+    private int minerals;
+    private int energy;
+    private int research;
+    private int alloys;
+
+}
---
+++ src/main/java/sk/samuel/projects/intergalacticempires/entities/Species.java
@@ -0,0 +1,7 @@
+package sk.samuel.projects.intergalacticempires.entities;
+
+public class Species {
+    private String species;
+    private int totalPopulationOfSpecies;
+
+}
