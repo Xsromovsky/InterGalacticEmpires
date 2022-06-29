bc24748e5a6f48f8965cb597bfcf9626ae02fe07
samuel <samuel14sromovsky@gmail.com>
2022-06-29 23:01:45 +0200

    Initial commit

---
+++ src/main/java/sk/samuel/projects/intergalacticempires/InterGalacticEmpiresApplication.java
@@ -0,0 +1,13 @@
+package sk.samuel.projects.intergalacticempires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterGalacticEmpiresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterGalacticEmpiresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/samuel/projects/intergalacticempires/InterGalacticEmpiresApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.samuel.projects.intergalacticempires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterGalacticEmpiresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
